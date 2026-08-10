9407af40e518a6e37b41659149d723594cf77d22
Sandeep <sandeep324.d@gmail.com>
2026-08-10 22:15:23 +0530

    moved component  bootstrapping to single  file

--- angular-complete-guide-course-resources-main/attachments/02-essentials/01-starting-project/src/main.ts
+++ angular-complete-guide-course-resources-main/attachments/02-essentials/01-starting-project/src/main.ts
@@ -4,5 +4,3 @@ import { AppComponent } from './app/app.component';
 import { HeaderComponent } from './app/header.component';
 
 bootstrapApplication(AppComponent).catch((err) => console.error(err));
-
-bootstrapApplication(HeaderComponent).catch((err) => console.error(err));
