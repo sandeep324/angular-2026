2faa6f22ead239007192775b4a4a34428b819a4c
Sandeep <sandeep324.d@gmail.com>
2026-08-11 07:36:38 +0530

    input decorator & passing values from parent to child component

--- angular-complete-guide-course-resources-main/attachments/02-essentials/01-starting-project/src/app/user/user.component.ts
+++ angular-complete-guide-course-resources-main/attachments/02-essentials/01-starting-project/src/app/user/user.component.ts
@@ -1,7 +1,6 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, computed, Input, signal } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
@@ -12,16 +11,16 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 })
 export class UserComponent {
 
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
+ 
+  @Input() avatar!:string;
+  @Input() name !: string;
 
   get userPath(){
-    //return 'assets/users/' + this.selectedUser().avatar;
-    return computed(()=>'assets/users/' + this.selectedUser().avatar);
+    return "assets/users/" + this.avatar;
   }
 
   onUserSelected() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+
 }
 
 }
