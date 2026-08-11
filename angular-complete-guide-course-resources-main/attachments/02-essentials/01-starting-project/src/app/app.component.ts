eff7847e6b1394f385ddfc45a4b512b0efd9d4d3
Sandeep <sandeep324.d@gmail.com>
2026-08-11 21:42:58 +0530

    passing values between components using eventemiiter & property binding - input , output

--- angular-complete-guide-course-resources-main/attachments/02-essentials/01-starting-project/src/app/app.component.ts
+++ angular-complete-guide-course-resources-main/attachments/02-essentials/01-starting-project/src/app/app.component.ts
@@ -3,14 +3,29 @@ import { HeaderComponent } from './header/header.component';
 import { UserComponent } from "./user/user.component";
 
 import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent,TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
+
   DUMMY_USERS = DUMMY_USERS;
+
+
+ selectedUser: any = "u1";
+
+ userSelected(id: string) {
+   this.selectedUser = id;
+}
+
+get getUser(){
+  return DUMMY_USERS.find(user => user.id === this.selectedUser) !;
+}
+
+
 }
--- angular-complete-guide-course-resources-main/attachments/02-essentials/01-starting-project/src/app/user/user.component.ts
+++ angular-complete-guide-course-resources-main/attachments/02-essentials/01-starting-project/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, Input, signal } from '@angular/core';
+import { Component, computed, EventEmitter, Input, Output, signal } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 
@@ -12,15 +12,18 @@ import { DUMMY_USERS } from '../dummy-users';
 export class UserComponent {
 
  
+  @Input({required:true}) id!:string;
   @Input({required:true}) avatar!:string;
   @Input({required:true}) name !: string;
+
+  @Output() selectedUser = new EventEmitter<string>();
 
   get userPath(){
     return "assets/users/" + this.avatar;
   }
 
   onUserSelected() {
-
-}
+    this.selectedUser.emit(this.id);
+  }
 
 }
