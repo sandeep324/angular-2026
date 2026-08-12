f03dfdc6c26cde3eb5ae1559089fbb7496259d75
Sandeep <sandeep324.d@gmail.com>
2026-08-12 23:44:12 +0530

    task componet added

---
+++ angular-complete-guide-course-resources-main/attachments/02-essentials/01-starting-project/src/app/tasks/task/task.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-task',
+  standalone: true,
+  imports: [],
+  templateUrl: './task.component.html',
+  styleUrl: './task.component.css'
+})
+export class TaskComponent {
+
+  @Input({required:true}) task!: { id: string; title: string; summary: string; dueDate: string; };
+
+}
--- angular-complete-guide-course-resources-main/attachments/02-essentials/01-starting-project/src/app/tasks/tasks.component.ts
+++ angular-complete-guide-course-resources-main/attachments/02-essentials/01-starting-project/src/app/tasks/tasks.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { TaskComponent } from "./task/task.component";
 
 
 interface User{
@@ -9,12 +10,43 @@ interface User{
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [],
+  imports: [TaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
 
   @Input({required:true}) selectedUser!: User;
+
+
+ dummyTasks = [
+  {
+    id: 't1',
+    userId: 'u1',
+    title: 'Master Angular',
+    summary:
+      'Learn all the basic and advanced features of Angular & how to apply them.',
+    dueDate: '2025-12-31',
+  },
+  {
+    id: 't2',
+    userId: 'u3',
+    title: 'Build first prototype',
+    summary: 'Build a first prototype of the online shop website',
+    dueDate: '2024-05-31',
+  },
+  {
+    id: 't3',
+    userId: 'u3',
+    title: 'Prepare issue template',
+    summary:
+      'Prepare and describe an issue template which will help with project management',
+    dueDate: '2024-06-15',
+  },
+]
+
+get selectedTasks(){
+  return this.dummyTasks.filter(task => task.userId === this.selectedUser.id);
+}
 
 }
